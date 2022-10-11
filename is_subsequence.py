def3134bf172f18524fc02fec7a655e9b64ef5fc
ra7kar <ra7kar@gmail.com>
2022-10-10 22:19:47 -0500

    Is a string a sub sequence of another string

---
+++ is_subsequence.py
@@ -0,0 +1,41 @@
+# give a string check if its a subsequence of another string
+# LeetCode problem
+# https://leetcode.com/problems/is-subsequence/
+
+# lets implement with the TDD approach, Test driven development
+
+import unittest
+from numpy import s_
+from numpy.testing._private.parameterized import parameterized
+
+class TestIsSubsequence(unittest.TestCase):
+
+    @parameterized.expand([ 
+        ('abc', 'dxzaddtdbc', True),
+        ('jfk', 'asdrjfllasdfkadsaf', True),
+        ('jfk', 'jasdf', False),
+    ])
+    def test_is_subsequence(self, s, t, result):
+        self.assertEqual(is_subsequence(s, t), result)
+
+def is_subsequence(s, t):
+    s_len = len(s)
+    t_len = len(t)
+
+    if s_len > t_len:
+        return False
+    if s_len == 0:
+        return True
+
+    pointer = 0
+
+    for c in t:
+        if c == s[pointer]:
+            pointer += 1
+            if pointer >= s_len:
+                break
+    
+    return pointer == s_len
+
+if __name__ == "__main__":
+    unittest.main()
